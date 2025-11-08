2f57ba60b7b15fa46d151ade3d5b038245186b43
Bytesbanana <saharat.paynok@gmail.com>
2025-11-08 15:52:39 +0700

    Refactor aws_subnet resource formatting for consistency

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ resource "aws_vpc" "nextwork-vpc" {
 }
 
 resource "aws_subnet" "nextwork-public-subnet-1" {
-  vpc_id = aws_vpc.nextwork-vpc.id
-  cidr_block = "10.0.0.0/24"
-  
-  
+  vpc_id                  = aws_vpc.nextwork-vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
   tags = {
-    Name: "Public 1"
+    Name : "Public 1"
   }
 }
