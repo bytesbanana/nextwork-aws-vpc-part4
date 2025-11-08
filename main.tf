f0a83928a48a8fffaaf9c7008c7f7bd4fcab4b46
Bytesbanana <saharat.paynok@gmail.com>
2025-11-08 17:17:15 +0700

    Add default route table and association for public subnet

--- main.tf
+++ main.tf
@@ -22,8 +22,27 @@ resource "aws_subnet" "nextwork-public-subnet-1" {
 
 resource "aws_internet_gateway" "nextwork-ig" {
   vpc_id = aws_vpc.nextwork-vpc.id
-  
   tags = {
     Name : "NextWork IG"
   }
+}
+
+
+
+resource "aws_default_route_table" "nextwork-vpc" {
+  default_route_table_id = aws_vpc.nextwork-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nextwork-ig.id
+  }
+
+  tags = {
+    Name = "NextWork route table"
+  }
+}
+
+resource "aws_route_table_association" "nextwork-public-subnet-1-association" {
+  subnet_id      = aws_subnet.nextwork-public-subnet-1.id
+  route_table_id = aws_default_route_table.nextwork-vpc.id
 }
