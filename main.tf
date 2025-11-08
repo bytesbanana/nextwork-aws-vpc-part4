17f10407f42a07f4adc3e012a0a67b3ccefd6355
Bytesbanana <saharat.paynok@gmail.com>
2025-11-08 19:00:08 +0700

    feat: add AWS resources for EC2 instance setup

--- main.tf
+++ main.tf
@@ -121,3 +121,38 @@ resource "aws_network_acl" "nextwork_private_subnet_NACL" {
     Name = "NextWork Private NACL"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.9.20251105.0-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "nextwork-ec2-key"
+  public_key = file("~/.ssh/id_rsa_nextwork_ec2.pub")
+}
+
+resource "aws_instance" "nextwork_public_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.ec2_key_pair.key_name
+  vpc_security_group_ids      = [aws_security_group.nextwork_public_security_group.id]
+  subnet_id                   = aws_subnet.nextwork_public_subnet_1.id
+  associate_public_ip_address = true
+
+
+  tags = {
+    Name = "NextWork Public Server"
+  }
+}
