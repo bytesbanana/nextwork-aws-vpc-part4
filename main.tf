f251a32d214d69bea5bfdc550256c8ad3f382eda
Bytesbanana <saharat.paynok@gmail.com>
2025-11-08 17:46:30 +0700

    Refactor resource names for consistency and add security group with ingress rule for HTTP access

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "nextwork-vpc" {
+resource "aws_vpc" "nextwork_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -10,8 +10,8 @@ resource "aws_vpc" "nextwork-vpc" {
   }
 }
 
-resource "aws_subnet" "nextwork-public-subnet-1" {
-  vpc_id                  = aws_vpc.nextwork-vpc.id
+resource "aws_subnet" "nextwork_public_subnet_1" {
+  vpc_id                  = aws_vpc.nextwork_vpc.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
 
@@ -20,8 +20,8 @@ resource "aws_subnet" "nextwork-public-subnet-1" {
   }
 }
 
-resource "aws_internet_gateway" "nextwork-ig" {
-  vpc_id = aws_vpc.nextwork-vpc.id
+resource "aws_internet_gateway" "nextwork_ig" {
+  vpc_id = aws_vpc.nextwork_vpc.id
   tags = {
     Name : "NextWork IG"
   }
@@ -29,12 +29,12 @@ resource "aws_internet_gateway" "nextwork-ig" {
 
 
 
-resource "aws_default_route_table" "nextwork-vpc" {
-  default_route_table_id = aws_vpc.nextwork-vpc.default_route_table_id
+resource "aws_default_route_table" "nextwork_vpc" {
+  default_route_table_id = aws_vpc.nextwork_vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nextwork-ig.id
+    gateway_id = aws_internet_gateway.nextwork_ig.id
   }
 
   tags = {
@@ -42,7 +42,25 @@ resource "aws_default_route_table" "nextwork-vpc" {
   }
 }
 
-resource "aws_route_table_association" "nextwork-public-subnet-1-association" {
-  subnet_id      = aws_subnet.nextwork-public-subnet-1.id
-  route_table_id = aws_default_route_table.nextwork-vpc.id
+resource "aws_route_table_association" "nextwork_public_subnet_1_association" {
+  subnet_id      = aws_subnet.nextwork_public_subnet_1.id
+  route_table_id = aws_default_route_table.nextwork_vpc.id
+}
+
+resource "aws_security_group" "nextwork_public_security_group" {
+  name        = "NextWork Public Security Group"
+  vpc_id      = aws_vpc.nextwork_vpc.id
+  description = "A Security Group for the NextWork VPC Public Subnet"
+
+  tags = {
+    Name = "NextWork Public Security Group"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4_from_anywhere" {
+  security_group_id = aws_security_group.nextwork_public_security_group.id
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
 }
